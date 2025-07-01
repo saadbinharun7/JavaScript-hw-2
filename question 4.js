353a6f4ebffcbde730458fdad6133ee6826f32fb
Pha raoh <pharaoh1822005@gmail.com>
2025-07-01 16:57:09 +0600

    question 4 complete

--- question 4.js
+++ question 4.js
@@ -1,3 +1,15 @@
 /*4.Create a function canVote that takes two parameters: age and citizenship
 * If age is 18 or older and citizenship is "Bangladeshi", print "You are eligible to vote."
 * Otherwise, print "You are not eligible to vote." */
+
+function canVote(age, citizenship){
+    if(age > 17 && citizenship == "Bangladeshi"){
+        return "You are eligiable to vote";
+    }
+    else{
+        return "You are not eligible to vote";
+    }
+}
+
+let result = canVote(18, "Bangladeshi");
+console.log(result);
