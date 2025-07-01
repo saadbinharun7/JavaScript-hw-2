cc54d01030a13366749fa631c5d15bf066947c0f
Pha raoh <pharaoh1822005@gmail.com>
2025-07-01 17:00:50 +0600

    question 5 complete

---
+++ question 5.js
@@ -0,0 +1,10 @@
+/* 5. Create an array called fruits with at least 5 fruit names. Print:
+
+* The whole array
+* The number of items in the array */
+
+const fruits = ["apple", "banana", "orange", "grape", "mango"];
+
+console.log(fruits);
+
+console.log("Number of items in the array:", fruits.length);
